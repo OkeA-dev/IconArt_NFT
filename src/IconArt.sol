a34c2381201925a4e501cb3e43b7f450194ca504
Oke.A <Okea.dev@gmail.com>
2024-07-08 15:23:24 +0100

    ...

--- src/IconArt.sol
+++ src/IconArt.sol
@@ -1,10 +1,25 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import {ERC721} is 
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract IconArt is ERC721 {
-    constructor() ERC("IconArt", "IAT") {
+    uint256 public s_tokenCounter;
+    mapping(uint256 => string) private s_tokenIdToUri;
 
+    constructor() ERC721("IconArt", "IAT") {
+        s_tokenCounter = 0;
+    }
+
+    function _mint(string memory _tokenUri) public {
+        s_tokenIdToUri[s_tokenCounter] = _tokenUri;
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter++;
+    }
+
+    function tokenURI(
+        uint256 _tokenId
+    ) public view override returns (string memory) {
+        return s_tokenIdToUri[_tokenId];
     }
 }
